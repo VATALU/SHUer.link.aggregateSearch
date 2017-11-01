3938c591977e6ab175c286d322aa4df7c6221b6d
VATALU <2507978950@qq.com>
2017-11-01 17:19:20 +0800

    [fix]fixed ZhiwangCrawlerTest mistake

--- src/test/java/test/org/shuerlink/crawler/ZhiwangCrawlerImplTest.java
+++ src/test/java/test/org/shuerlink/crawler/ZhiwangCrawlerImplTest.java
@@ -7,16 +7,10 @@ import org.junit.Test;
 import org.shuerlink.crawlerImpl.ZhiwangCrawlerImpl;
 
 public class ZhiwangCrawlerImplTest {
-	@Test
-	public void testBaiduCrawler() throws UnsupportedEncodingException {
+    @Test
+    public void testBaiduCrawler() throws UnsupportedEncodingException {
 
-	System.out.println("Start:");
-	try {
-		System.out.println(new ZhiwangCrawlerImpl().start("王宝强"));
-	} catch (IOException e) {
-		e.printStackTrace();
-		System.out.println("connect failer");
-	}
-	System.out.println("End~");
-}
+        System.out.println("Start:");
+        System.out.println(new ZhiwangCrawlerImpl().getPaperReult("王宝强"));
+    }
 }
