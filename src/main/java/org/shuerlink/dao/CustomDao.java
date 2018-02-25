d2ce6941d6b1ee55e9f5475dde8483cbd893353b
VATALU <2507978950@qq.com>
2018-02-25 20:54:49 +0800

    [delete]

--- src/main/java/org/shuerlink/dao/CustomDao.java
+++
@@ -1,8 +0,0 @@
-package org.shuerlink.dao;
-
-import org.shuerlink.model.Student.Custom;
-
-public interface CustomDao {
-    public Custom findOneById(String id);
-    public void save(Custom custom);
-}
--- src/main/java/org/shuerlink/service/SHULoginService.java
+++
@@ -1,7 +0,0 @@
-package org.shuerlink.service;
-
-import org.shuerlink.model.Student.StudentInfo;
-
-public interface SHULoginService {
-    public StudentInfo loginSHUStudent (String userName, String password);
-}
